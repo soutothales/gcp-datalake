0e31b206444eab895c5ecaf338b8d7c28e27adee
Thales Souto <thaleshenriquepb@gmail.com>
2022-06-08 22:03:48 -0300

    Testing API calls and saving dump to data folder

--- requests/routes.py
+++ requests/routes.py
@@ -1,1 +1,17 @@
 # This file will include the routes that will be called by the API
+
+# Import section
+import requests as requests
+import json as json
+
+import sys as sys
+sys.path.append( '..' )
+import constants as constants
+
+
+auth_header = {"X-Auth-Token": constants.TOKEN_API}
+response = requests.get(constants.TEAMS_URL, headers=auth_header)
+response_json = response.json()
+
+with open(constants.TEAMS_DATA_JSON_PATH, "w") as outfile:
+    json.dump(response_json, outfile)

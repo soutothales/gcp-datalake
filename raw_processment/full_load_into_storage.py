8a61b8aa8864341832c7c4ecce2b66ebccea1c28
Thales Souto <thaleshenriquepb@gmail.com>
2022-06-10 09:50:16 -0300

    First (raw/json) file upload to GCS

--- raw_processment/full_load_into_storage.py
+++ raw_processment/full_load_into_storage.py
@@ -1,1 +1,23 @@
-# This file will be responsible for loading data into storage service 
+# This file will be responsible for loading data into storage service
+
+import os
+from google.cloud import storage
+
+import sys as sys
+sys.path.append( '..' )
+import constants as constants
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../{}".format(constants.GCS_API_KEY_FILE)
+
+storage_client = storage.Client()
+
+chosen_bucket = storage_client.get_bucket(constants.GCS_BUCKET_NAME)
+
+filename = constants.TEAMS_DATA_FILE
+
+upload_file = os.path.join("../data/", filename)
+
+blob = chosen_bucket.blob("raw/" + filename)
+
+blob.upload_from_filename(upload_file)
+

46a7ddad1eb47673fa93a05e5bd2d6d5d291dfd2
Thales Souto <thaleshenriquepb@gmail.com>
2022-06-10 10:09:29 -0300

    Refactor code into a function (try/exception)

--- raw_processment/full_load_into_storage.py
+++ raw_processment/full_load_into_storage.py
@@ -9,15 +9,20 @@ import constants as constants
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "../{}".format(constants.GCS_API_KEY_FILE)
 
-storage_client = storage.Client()
-
-chosen_bucket = storage_client.get_bucket(constants.GCS_BUCKET_NAME)
-
+bucket_name = constants.GCS_BUCKET_NAME
 filename = constants.TEAMS_DATA_FILE
 
-upload_file = os.path.join("../data/", filename)
+def upload_file(bucket_name, file_name):
+    try:
+        storage_client = storage.Client()
+        chosen_bucket = storage_client.get_bucket(bucket_name)
+        upload_file = os.path.join("../data/", file_name)
+        blob = chosen_bucket.blob("raw/" + file_name)
+        blob.upload_from_filename(upload_file)
 
-blob = chosen_bucket.blob("raw/" + filename)
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
-blob.upload_from_filename(upload_file)
-
+upload_file(bucket_name, filename)
